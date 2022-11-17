dcfc79c98933a5f16cfba58ba886f59d3bc311b6
potatoalienof13 <51922130+potatoalienof13@users.noreply.github.com>
2022-11-17 10:12:21 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -20,6 +20,7 @@ scene.cameraFollowSprite(mySprite)
 controller.moveSprite(mySprite)
 info.setScore(0)
 info.setLife(3)
+controller.player2.moveSprite(mySprite)
 tiles.setCurrentTilemap(tilemap`Test Arena`)
 game.onUpdate(function () {
 	
