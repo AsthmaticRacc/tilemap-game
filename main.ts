4df719426daaa53d20ab508f7a26aca9ef33009c
potatoalienof13 <51922130+potatoalienof13@users.noreply.github.com>
2022-11-21 10:04:26 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,45 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    vectorx2 = controller.dx()
-    vectory2 = controller.dy()
+    mySprite.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 2 2 2 . . . . . . . 
+        . . . . . 2 2 2 2 2 . . . . . . 
+        . . . . . 2 3 2 3 2 . . . . . . 
+        . . . . . 3 2 2 2 3 . . . . . . 
+        . . . . . 3 3 3 3 3 . . . . . . 
+        . . . . . 2 2 2 2 2 . . . . . . 
+        . . . . . . . 3 . . . . . . . . 
+        . . . . . 2 2 2 2 2 . . . . . . 
+        . . . . . 2 2 2 2 2 . . . . . . 
+        . . . . . 2 2 2 2 2 . . . . . . 
+        . . . . . 2 2 2 2 2 . . . . . . 
+        . . . . . 3 . 3 . 3 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    inDash = true
+    timer.after(100, function () {
+        mySprite.setImage(assets.image`Cooldown`)
+        mySprite.setVelocity(0, 0)
+        inDash = false
+    })
+    timer.after(500, function () {
+        mySprite.setImage(assets.image`Normal`)
+    })
+    vectorx = controller.dx()
+    vectory = controller.dy()
     if (vectorx2 != 0 && vectory2 != 0) {
         vectorx2 /= 1.41421356
 vectory2 /= 1.41421356
     }
-    mySprite.setVelocity(10000 * vectorx * 0.5 + mySprite.vx * 0.5, 10000 * vectory * 0.5 + mySprite.vy * 0.5)
+    mySprite.setVelocity(999999 * vectorx * 0.5 + mySprite.vx * 0.5, 999999 * vectory * 0.5 + mySprite.vy * 0.5)
 })
-let vectory = 0
-let vectorx = 0
-let vectory2 = 0
-let vectorx2 = 0
+let inDash = false
 let mySprite: Sprite = null
+let vectorx2 = 0
+let vectory2 = 0
+let vectorx = 0
+let vectory = 0
 mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -37,6 +65,9 @@ tiles.setCurrentTilemap(tilemap`Test Arena`)
 game.onUpdate(function () {
     vectorx = controller.dx()
     vectory = controller.dy()
+    if (inDash) {
+        return;
+    }
     if (vectorx != 0 && vectory != 0) {
         vectorx /= 1.41421356
 vectory /= 1.41421356
