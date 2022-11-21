3afab8980771f48b316f37048a06a3ac1d56366e
potatoalienof13 <51922130+potatoalienof13@users.noreply.github.com>
2022-11-21 09:47:56 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    vectorx2 = controller.dx()
+    vectory2 = controller.dy()
+    if (vectorx2 != 0 && vectory2 != 0) {
+        vectorx2 /= 1.41421356
+vectory2 /= 1.41421356
+    }
+})
+let vectory = 0
+let vectorx = 0
+let vectory2 = 0
+let vectorx2 = 0
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -16,12 +28,16 @@ let mySprite = sprites.create(img`
     . . . . . 3 . 3 . 3 . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-mySprite.setVelocity(50, 50)
 scene.cameraFollowSprite(mySprite)
-controller.moveSprite(mySprite)
 info.setScore(0)
 info.setLife(3)
 tiles.setCurrentTilemap(tilemap`Test Arena`)
 game.onUpdate(function () {
-	
+    vectorx = controller.dx()
+    vectory = controller.dy()
+    if (vectorx != 0 && vectory != 0) {
+        vectorx /= 1.41421356
+vectory /= 1.41421356
+    }
+    mySprite.setVelocity(70 * vectorx * 0.5 + mySprite.vx * 0.5, 70 * vectory * 0.5 + mySprite.vy * 0.5)
 })
