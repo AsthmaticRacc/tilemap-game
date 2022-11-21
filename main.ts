2270e7d1bd165a7a41ff38d57453756cbef4fe99
potatoalienof13 <51922130+potatoalienof13@users.noreply.github.com>
2022-11-21 09:44:33 -0500

    Restore '11/18/2022, 9:47:00 AM Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -18,29 +18,10 @@ let mySprite = sprites.create(img`
     `, SpriteKind.Player)
 mySprite.setVelocity(50, 50)
 scene.cameraFollowSprite(mySprite)
-//controller.moveSprite(mySprite)
+controller.moveSprite(mySprite)
 info.setScore(0)
 info.setLife(3)
 tiles.setCurrentTilemap(tilemap`Test Arena`)
 game.onUpdate(function () {
-    let vectorx= controller.dx()
-    let vectory = controller.dy()
-
-    if(vectorx != 0 && vectory != 0){
-        vectorx /= 1.41421356
-        vectory /= 1.41421356
-
-    }
-	mySprite.setVelocity(70 * vectorx * 0.5 + mySprite.vx * 0.5, 70 * vectory * 0.5 + mySprite.vy * 0.5)
-})
-controller.A.onEvent(ControllerButtonEvent.Pressed, function() {
-    let vectorx = controller.dx()
-    let vectory = controller.dy()
-
-    if (vectorx != 0 && vectory != 0) {
-        vectorx /= 1.41421356
-        vectory /= 1.41421356
-
-    }
-
+	
 })
